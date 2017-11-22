38f4d1f716792ece07c64db69921cc6c3bb8293e
Seth Sandaru <sethsandaru@seths-mac.local>
2017-11-22 07:44:03 -0800

    Initial Project

---
+++ BDSNhom6/BDSNhom6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BDSNhom6
+//
+//  Created by Seth Sandaru on 11/22/17.
+//  Copyright © 2017 Nhom 6 Estate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
