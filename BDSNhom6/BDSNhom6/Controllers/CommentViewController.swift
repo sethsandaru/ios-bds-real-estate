e75dd755c60a29cc88fbaff8d6d9b4b4052aa64e
TIEN <tien@TIENs-Mac.local>
2017-12-16 17:25:36 +0700

    Thêm CommentView vào StoryBoard

---
+++ BDSNhom6/BDSNhom6/Controllers/CommentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CommentViewController.swift
+//  BDSNhom6
+//
+//  Created by TIEN on 12/16/17.
+//  Copyright © 2017 Nhom 6 Estate. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+
+    @IBOutlet weak var commentTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
